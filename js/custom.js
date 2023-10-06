8df7a4f4c105acf7026e5b5c8f235602b434e430
churanta <churantamondal321@gmail.com>
2023-10-06 12:37:47 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,131 @@
+// Format SatSchedule
+class scheduleEvent {
+    constructor(title, start, end, track) {
+        this.title = title;
+        this.startTime = start;
+        
+        if (track == undefined){
+          this.end = null
+          this.track = end;
+        }
+        else{
+          this.endTime = end;
+          this.track = track;
+        }
+        
+    }
+
+}
+
+var saturday = [];
+var sunday = [];
+
+// track = {main, ws} where ws is workshop
+saturday.push(new scheduleEvent("", "9:00", "main"));
+saturday.push(new scheduleEvent("", "9:30", "main"));
+saturday.push(new scheduleEvent("", "11:00", "main"));
+saturday.push(new scheduleEvent("", "12:15", "13:15", "main"));
+saturday.push(new scheduleEvent("", "13:00", "main"));
+saturday.push(new scheduleEvent("", "13:30", "14:30", "main"));
+saturday.push(new scheduleEvent("", "15:00", "16:00", "main"));
+saturday.push(new scheduleEvent("", "17:00", "18:00", "main"));
+saturday.push(new scheduleEvent("", "19:00", "main"));
+// sunday.push(new scheduleEvent("No workshops today", "00:00", "17:00", "ws"));
+
+saturday.sort((a,b) => (a.startTime >= b.startTime) ? 1: -1);
+sunday.sort((a,b) => (a.startTime >= b.startTime) ? 1 : -1 );
+
+var str = '<tbody>';
+str +=  '<tr><th></th><th>Main track</th></tr>';
+saturday.forEach(function(ev, index){
+  if (index ==0  || saturday[index-1].startTime != ev.startTime){     
+    str += '<tr>';
+    if (ev.track == 'ws'){
+
+        str += '<th></th>';
+        str += '<td></td>';
+    }
+
+  str += '<th>';
+
+  str += ev.startTime 
+  if (ev.endTime){
+    str+='<br />|<br />' + ev.endTime;
+  }
+  str += '</th>';
+  str += '<td>';
+  str += ev.title + '';
+  str += '</td>';
+  
+  if (ev.track == 'main'){
+      if(index != saturday.length-1 && ev.startTime == saturday[index+1].startTime){
+        str += '<th>';
+        str+=saturday[index+1].startTime
+        if(saturday[index+1].endTime){
+        str +=  '<br />|<br />' + saturday[index+1].endTime;
+      }
+    
+  str += '</th>';
+  str += '<td>' + saturday[index+1].title+ '</td>';
+    }
+  else{
+  str +='<th></th>';
+  str += '<td></td>';
+  }}
+  
+  str += '</tr>';
+  
+  }
+
+});
+str += '</tbody>';
+window.document.getElementById("saturdayContainer").innerHTML = str;
+
+var str = '<tbody>';
+str +=  '<tr><th></th><th>Main track</th><th></tr>';
+sunday.forEach(function(ev, index){
+  if (index ==0  || (sunday[index-1].startTime != ev.startTime || sunday[index-1].track == ev.track) ){     
+    str += '<tr>';
+    if (ev.track == 'ws'){
+
+        str += '<th></th>';
+        str += '<td></td>';
+    }
+    
+    
+
+  str += '<th>';
+
+  str += ev.startTime 
+  if (ev.endTime){
+    str+='<br />|<br />' + ev.endTime;
+  }
+  str += '</th>';
+  str += '<td>';
+  str += ev.title + '';
+  str += '</td>';
+  
+  if (ev.track == 'main'){
+    if (index != sunday.length-1 && ev.startTime == sunday[index+1].startTime && ev.track != sunday[index+1].track){
+        str += '<th>';
+        str+=sunday[index+1].startTime
+        if(sunday[index+1].endTime){
+        str +=  '<br />|<br />' + sunday[index+1].endTime;
+      }
+    
+  str += '</th>';
+  str += '<td>' + sunday[index+1].title+ '</td>';
+    
+  }
+  else{
+  str +='<th></th>';
+  str += '<td></td>';
+  }}
+  
+  str += '</tr>';
+  
+  }
+
+});
+str += '</tbody>';
+window.document.getElementById("sundayContainer").innerHTML = str;
